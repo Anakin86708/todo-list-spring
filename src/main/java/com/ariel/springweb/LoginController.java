dda59d8c6fc4ff4d8604e0ba0aa50fed5d0e2c23
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-12 12:10:17 -0300

    Recebendo nome em GET e exibindo no HTML.

--- src/main/java/com/ariel/springweb/LoginController.java
+++ src/main/java/com/ariel/springweb/LoginController.java
@@ -1,13 +1,16 @@
 package com.ariel.springweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    public String loginMessage() {
+    public String loginMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
         return "login";
     }
 }
