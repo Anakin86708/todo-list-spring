359da1409910199f91c3ccaab4c7383421dbd79d
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-12 12:55:56 -0300

    Welcome com uso de POST.

--- src/main/java/com/ariel/springweb/LoginController.java
+++ src/main/java/com/ariel/springweb/LoginController.java
@@ -3,14 +3,20 @@ package com.ariel.springweb;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginMessage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String loginMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "login";
+        return "welcome";
     }
 }
