7630ed61a3027bfcf43df70e15903a08bded9923
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-13 09:30:39 -0300

    Adicionando ToDos.

--- src/main/java/com/ariel/springweb/controller/TodoController.java
+++ src/main/java/com/ariel/springweb/controller/TodoController.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @Controller
 @SessionAttributes("user")
@@ -18,8 +22,24 @@ public class TodoController {
     @RequestMapping(value = "/todo-list", method = RequestMethod.GET)
     public String showTodoPage(Model model) {
         String user = (String) model.getAttribute("user");
-        model.addAttribute("todos", service.getTodosFromUser(user));
-        model.addAttribute("user", user);
+        refreshModelTodos(model, user);
         return "todo-list";
+    }
+
+    @RequestMapping(value = "/add-todo", method = RequestMethod.GET)
+    public String showAddNewTodo(Model model) {
+        return "add-todo";
+    }
+
+    @RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+    public String addNewTodo(@RequestParam String desc, @RequestParam String targetDate, @RequestParam(value = "isDone", defaultValue = "false") boolean isDone, Model model) throws ParseException {
+        String user = (String) model.getAttribute("user");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd");
+        service.addTodo(user, desc, dateFormat.parse(targetDate), isDone);
+        return "redirect:/todo-list";
+    }
+
+    private void refreshModelTodos(Model model, String user) {
+        model.addAttribute("todos", service.getTodosFromUser(user));
     }
 }
